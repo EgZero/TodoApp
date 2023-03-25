a557cc3b026cafcd401ad94243f50315a5732753
Angel <Angelorellana664@gmail.com>
2023-03-25 12:57:54 -0500

    Clase 03 Completada

--- TODO App/assets/JS/Index.js
+++ TODO App/assets/JS/Index.js
@@ -1,9 +1,24 @@
 const createBtn = document.querySelector("[data-createBtn]");
 const createInput = document.querySelector("[data-createInput]");
+const taskCard = document.querySelector("[data-taskContainer]");
 
 const createTask = (event) => {
     event.preventDefault();
-    console.log(createInput.value);
+    
+    const textTask = createInput.value;
+    const cardList = document.querySelector("[data-cardList]")
+    const cardItem = document.createElement("li");
+
+    createInput.value = "";
+    const cardContent = `<div>
+                    <i class="far fa-check-square icon"></i>
+                    <span class="task">${textTask}</span>
+                    </div>
+                    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    cardItem.classList.add("card")
+    cardItem.innerHTML = cardContent;
+
+    cardList.appendChild(cardItem);
 }
 
 createBtn.addEventListener("click", createTask);
