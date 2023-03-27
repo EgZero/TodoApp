03059064ea08fd2da734859616de6e5cea64c635
Angel <Angelorellana664@gmail.com>
2023-03-26 22:01:20 -0500

    Clase 05 Terminada

--- TODO App/assets/JS/Index.js
+++ TODO App/assets/JS/Index.js
@@ -2,36 +2,32 @@
     
 const createBtn = document.querySelector("[data-createBtn]");
 const createInput = document.querySelector("[data-createInput]");
-const taskCard = document.querySelector("[data-taskContainer]");
+const cardList = document.querySelector("[data-cardList]");
 
 const createTask = (event) => {
     event.preventDefault();
 
     const textTask = createInput.value;
-    const cardList = document.querySelector("[data-cardList]")
     const cardItem = document.createElement("li");
-    cardItem.classList.add("card")
+    cardItem.classList.add("card");
 
     const cardContent= document.createElement("div");
-    cardContent.appendChild(checkoutTask());
+    cardContent.appendChild(checkoutIcon());
 
     const cardTitle = document.createElement("span");
     cardTitle.classList.add("task");
-    cardTitle.innerHTML = textTask;
+    cardTitle.textContent = textTask;
     cardContent.appendChild(cardTitle);
     cardItem.appendChild(cardContent);
 
     createInput.value = "";
-    const cardTextContent = `
-                    <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    cardItem.appendChild(cardContent);
-
+    cardItem.appendChild(removeIcon());
     cardList.appendChild(cardItem);
 }
 
 createBtn.addEventListener("click", createTask);
 
-const checkoutTask = ()=>{
+const checkoutIcon = ()=>{
     const i = document.createElement("i");
     i.classList.add("far" , "fa-check-square" , "icon")
     i.addEventListener("click" , checkTask)
@@ -43,5 +39,17 @@ const checkTask = (event)=>{
             element.classList.toggle("far")
             element.classList.toggle("fas")
             element.classList.toggle("completeIcon")
+}
+
+const removeIcon = () =>{
+    const i = document.createElement("i");
+    i.classList.add("fas" , "fa-trash-alt" , "trashIcon" , "icon")
+    i.addEventListener("click" , removeTask)
+    return i
+}
+
+const removeTask = (event) =>{
+    const element = event.target.parentElement
+    cardList.removeChild(element)
 }
 })()
